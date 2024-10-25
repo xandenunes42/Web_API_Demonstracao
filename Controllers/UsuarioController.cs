7f658dad4c40b43f30fd5da63941f7720cf0bddc
Alexandre Oliveira Nunes <alexandre_nunes@Alexandre.home>
2024-10-24 22:20:16 -0300

    Update Web_API_Demonstracao

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API_Demonstracao.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new 
+            { 
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Ola ! Me chamo {nome}";
+
+            return Ok(new {mensagem});
+        }
+    }
+
+}
